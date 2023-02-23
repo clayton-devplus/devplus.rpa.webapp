1a4c6e5a8f272e078d8afaf3fab456ae47c901b7
Clayton Oliveira <clayton@devplus.com.br>
2023-02-23 17:28:26 -0300

    melhorias certidao list

---
+++ src/app/smart/certidoes/certidoes-status/certidoes-status.component.ts
@@ -0,0 +1,63 @@
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+import { faGears, faUser } from "@fortawesome/free-solid-svg-icons";
+import { Certidao } from "../certidoes";
+import { CertidoesService } from "../certidoes.service";
+
+@Component({
+  selector:'dev-certidoes-status',
+  templateUrl:'./certidoes-status.component.html'
+})
+export class CertidoesStatusComponent {
+
+  faUser = faUser;
+  faGears = faGears;
+  certidaoEdit: Certidao | null = null;
+  certidaoForm: FormGroup;
+  encodedReport: string = '';
+  safeEncodedReport: SafeResourceUrl | null;
+
+  constructor (private formBuilder: FormBuilder,
+               private certidaoService: CertidoesService,
+               private sanitizer: DomSanitizer) {
+
+                this.safeEncodedReport = null;
+
+            //Construcao de formulario de empresa
+    this.certidaoForm = this.formBuilder.group({
+              id: [''],
+              datahoraemissao: [''],
+              datavalidade: [''],
+              cnpj: [''],
+              cstat: ['']
+
+          });
+
+  }
+
+  editCertidao(certidao: Certidao) {
+
+    if(certidao.filetype == 'png')
+      this.encodedReport = 'data:image/png;base64,' + certidao.arquivo;
+    else
+    {
+      this.safeEncodedReport = this.sanitizer.bypassSecurityTrustResourceUrl('data:application/pdf;base64,' + certidao.arquivo.toString());
+    }
+
+    this.certidaoEdit = certidao;
+
+    this.certidaoForm.reset();
+    this.certidaoForm.disable();
+
+    this.certidaoForm.controls['id'].setValue(certidao.id);
+
+    this.certidaoForm.controls['datahoraemissao'].setValue(certidao.datahoraemissao);
+    this.certidaoForm.controls['datavalidade'].setValue(certidao.datavalidade);
+    this.certidaoForm.controls['cnpj'].setValue(certidao.cnpj);
+    this.certidaoForm.controls['cstat'].setValue(certidao.cstat);
+
+
+  }
+
+}
--- src/app/smart/certidoes/certidoes.module.ts
+++ src/app/smart/certidoes/certidoes.module.ts
@@ -1,15 +1,23 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { CertidoesListComponent } from "./certidoes-list/certidoes-list.component";
+import { CertidoesStatusComponent } from "./certidoes-status/certidoes-status.component";
 
 @NgModule({
 
-    declarations:[CertidoesListComponent],
-    exports:[CertidoesListComponent],
+    declarations:[
+        CertidoesListComponent,
+        CertidoesStatusComponent
+    ],
+    exports:[
+        CertidoesListComponent
+    ],
     imports:[
         CommonModule,
-        FontAwesomeModule
+        FontAwesomeModule,
+        ReactiveFormsModule
     ]
 
 })
