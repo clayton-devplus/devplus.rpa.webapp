0ea894c15b87a0e1fafd3fbe7633971c0e46aa2e
Clayton Oliveira <claytondba@yahoo.com.br>
2023-03-06 20:47:36 -0300

    alts

--- src/app/smart/ginfes/ginfes-reproc/ginfes-reproc.component.ts
+++ src/app/smart/ginfes/ginfes-reproc/ginfes-reproc.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 import { Router } from "@angular/router";
 import { Pulse } from "src/app/core/proc/pulse";
 import { PulseService } from "src/app/core/proc/pulse.service";
@@ -12,11 +13,21 @@ import { GinfesService } from "../ginfes.service";
 export class GinfesReprocComponent { 
 
     @Input() ginfesReproc!: Ginfes | null;
-    ginfesOperators!: Pulse[];
+    ginfesOperators: Pulse[] = [];
+    reprocForm: FormGroup;
+    avancadoCheck = false;
 
     constructor(private ginfesService: GinfesService,
                 private pulseService: PulseService,
+                private formBuilder: FormBuilder,
                 private router: Router){
+
+                    this.reprocForm = this.formBuilder.group({
+                        instance_name: [''],
+                        proc_avancado:[false],
+              
+                    });
+                this.reprocForm.controls['proc_avancado'].setValue(false);
 
             pulseService.listOperatorsByService(4).subscribe(
                 ginfesOps => {
@@ -26,16 +37,44 @@ export class GinfesReprocComponent {
             )
 
     }
+    clear()
+    {
+        this.avancadoCheck = false;
+        this.reprocForm.controls['instance_name'].disable();
+        this.reprocForm.controls['proc_avancado'].setValue(false);
 
+    }
+    onCheck()
+    {
+        console.log('foi');
+        if(!this.avancadoCheck)
+            this.reprocForm.controls['instance_name'].enable();
+        else
+            this.reprocForm.controls['instance_name'].disable();
 
+        this.avancadoCheck = !this.avancadoCheck;
+    }
     reTry(){
 
         //this.disableReproc = true;
         //this.closeItera.nativeElement.click();
-        console.log(this.ginfesReproc);
-        // this.ginfesService.reTry(this.ginfesReproc!.id).subscribe(() => {
-        //     this.router.navigate(['smart','ginfes']);
-        // });
+        
+
+        if(this.avancadoCheck)
+        {
+            console.log('caiu avancado');
+            this.ginfesService.advancedReTry(this.ginfesReproc!.id, this.reprocForm.controls['instance_name'].value).subscribe(() => {
+                this.router.navigate(['smart','ginfes']);
+            });
+        }
+        else
+        {
+            console.log('caiu normal');
+            this.ginfesService.reTry(this.ginfesReproc!.id).subscribe(() => {
+                this.router.navigate(['smart','ginfes']);
+            });
+        }
+
     
       }
 }
--- src/app/smart/ginfes/ginfes.service.ts
+++ src/app/smart/ginfes/ginfes.service.ts
@@ -2,8 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Ginfes } from "./ginfes";
 
-const API = "https://rpa.devplus.com.br";
-//const API = "https://localhost:7109";
+//const API = "https://rpa.devplus.com.br";
+const API = "https://localhost:7109";
 
 @Injectable({
     providedIn:'root'
@@ -21,7 +21,11 @@ export class GinfesService {
     }
     reTry(id: number) {
       return this.http.get(API + '/ConsultaGinfes/Retry/' + id);
-  }
+    }
+    advancedReTry(id: number, instance: number) {
 
+        console.log('intance: ' + instance);
+        return this.http.get(API + '/ConsultaGinfes/Retry/' + id + '/' + instance);
+    }
 
 }
